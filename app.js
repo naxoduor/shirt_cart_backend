bb56a564aa80d37d398b1d6a2af57dc1cba9a359
oduormaradona <naxoduor7@gmail.com>
2019-08-16 14:38:53 +0300

    cleaning

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ db.authenticate()
 .catch(err => console.log('Error ' + err))
 const app = express();
 app.use(cors({
-    origin: 'http://104.248.73.139:3000/',
+    origin: 'http://104.248.73.139:3000',
     credentials: true,
   })
   )
@@ -25,82 +25,6 @@ app.get('/', (req,res) => res.send(`INDEX`));
 
 const PORT = process.env.PORT || 8080;
 
-Product.count ({
-  include: [{
-    model: Category,
-    where: { category_id: 2 },
-  }]
-}).then((count)=>{
-  let list=[]
-  let obj={}
-  obj.categories_count=count
-  list.push(obj)
-  console.log(JSON.stringify(list))
-})
-/*Order.create({
-  created_on: new Date(),
-  customer_id:"",
-  shipping_id:"",
-  tax_id:""
-}).then((order) =>{
-  let order_id = JSON.parse(JSON.stringify(order)).order_id
-ShoppingCart.findAll({
-  attributes: ['quantity', 'attributes', 'product_id'],
-  include: [{// Notice `include` takes an ARRAY
-          model: Product,
-          as: "products",
-          attributes: ['name', 'price']
-        }],
-        where: {
-          cart_id: "D4rwCMDqrsDsEM12rErBHI5sGEDo4BMJ"
-        }
-}).then((cart) => {
-  let total=0;
-  let itemsList=[]
-  cart.forEach((item, index) =>{
-    let good = JSON.parse(JSON.stringify(item))
-    let attributes=good.attributes
-    let obj={}
-    //console.log(item.getProducts())
-    let quantity=good.quantity
-    Product.findByPk(good.product_id).then((product) =>{
-      let productItem=JSON.parse(JSON.stringify(product))
-      let unit_cost=productItem.price
-      let subtotal=quantity*unit_cost
-      total=total+subtotal
-      obj.order_id=order_id
-      obj.product_id=productItem.product_id
-      obj.attributes=attributes
-      obj.product_name=productItem.name
-      obj.quantity=quantity
-      obj.unit_cost=unit_cost
-      itemsList.push(obj)
-      if (!cart[index + 1]) {
-        console.log(itemsList)
-        console.log("we bulk create here")
-      }
-    })
-  })
-})
-})*/
-/*
-Category.findAll().then(categories => {
-   categories.forEach((category) => {
-     console.log(category.getProducts.length)
-    category.getProducts().then((products) =>{
-     // console.log(products)
-      products.forEach((product) =>{
-       // console.log(product)
-        products1.push(product)
-      })
-    })
-  })  
-})
-console.log("print product1")
-console.log(products1)
-*/
-//console.log(products1)
-//Gig routes
 app.use('/products', require('./routes/products'))
 
 app.use('/categories', require('./routes/categories'))
