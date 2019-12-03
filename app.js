bf5b7eba6ff746884f1e41b2b668c46c0e77208f
oduormaradona <naxoduor7@gmail.com>
2019-12-03 14:39:49 +0300

    add cors policy credentials

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.use(function(req, res, next) {
   if(allowedOrigins.indexOf(origin) > -1){
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
   return next();
 })
 /*
