7bdc06bc9dd88decfac46cb8b79b9737b5eb3c5c
Nax Oduor <naxoduor7@gmail.com>
2024-08-13 09:35:19 +0300

    update info

--- config/product.js
+++ config/product.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 export const sequelize = new Sequelize(
     'energy',
     'root',
-    'password',
+    'Maradona@#',
     {
       host: 'localhost',
       dialect: 'mysql',
