70c99575026e224f38483995e61fb0d9f2e00c95
Nax Oduor <naxoduor7@gmail.com>
2025-07-30 22:29:43 +0300

    add categories

---
+++ category.sql
@@ -0,0 +1,47 @@
+-- Populate category table
+INSERT INTO `category` (`category_id`, `department_id`, `name`, `description`) VALUES
+    (1, 1, 'Solar', 'SolarMax Solar Panels'),
+    (2, 1, 'UPS', 'UPS'),
+    (3, 1, 'Battery', 'Battery'),
+    (4, 1, 'Extension', 'Extension'),
+    (5, 1, 'Socket', 'Socket'),
+    (6, 1, 'Surge Protector', 'Surge Protector'),
+    (7, 1, 'Bulb Holder', 'Bulb Holder'),
+    (8, 1, 'Circuit Breaker', 'Circuit Breaker'),
+    (9, 1, 'Switch', 'Switch'),
+    (10, 1, 'Junction box', 'Junction Box'),
+    (11, 1, 'Couplers', 'Couplers'),
+    (12, 1, 'Switch', 'Switch'),
+    (13, 1, 'Wire', 'Wire'),
+    (14, 1, 'Inverter', 'Inverter'),
+    (15, 1, 'Saw', 'Saw'),
+    (16, 1, 'Grinder', 'Grinder'),
+    (17, 1, 'Wrench', 'Wrench'),
+    (18, 1, 'Drill', 'Drill'),
+    (19, 1, 'Cable', 'Cable'),
+    (20, 1, 'Contactor', 'Contactor'),
+    (21, 1, 'Cooker', 'Cooker'),
+    (22, 1, 'Heater', 'Heater'),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
