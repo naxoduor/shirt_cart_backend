cebf78840c7c8662a944f86cf6f553aa5fd1935c
Nax Oduor <naxoduor7@gmail.com>
2025-08-02 22:26:08 +0300

    add yaml files

--- config/swagger.js
+++ config/swagger.js
@@ -2,6 +2,6 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 import swaggerUi from 'swagger-ui-express';
 
-const swaggerDocument = yaml.load(fs.readFileSync('../swagger.yaml', 'utf8'));
+const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
 
 export { swaggerUi, swaggerDocument };
