28937d6eeb8d6b8304958f4da6d8d7617875cc2b
Nax Oduor <naxoduor7@gmail.com>
2025-08-02 22:37:42 +0300

    edit yaml

--- config/swagger.js
+++ config/swagger.js
@@ -3,7 +3,7 @@ import path from 'path';
 import yaml from 'js-yaml';
 import swaggerUi from 'swagger-ui-express';
 
-const swaggerPath = path.resolve('swagger.yaml'); // Adjust if the file is in a subfolder
+const swaggerPath = path.resolve('config/swagger.yaml'); // Adjust if the file is in a subfolder
 const swaggerDocument = yaml.load(fs.readFileSync(swaggerPath, 'utf8'));
 
 
